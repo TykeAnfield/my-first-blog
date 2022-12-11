0a054b4e5199d450958f1616f40d6f427ddaf8b9
Tyke Anfield <tanfield1@ivytech.edu>
2022-12-11 11:04:52 -0800

    -m "total website modification"

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+@login_required
+def post_new(request):
+    [...]
+
+@login_required
+def post_edit(requests):
+    [...]
+
+@login_required
+def post_draft_list(requests):
+    [...]
+
+@login_required
+def post_remove(requests):
+    [...]
+
+@login_required
+def post_publish(requests):
+    [...]
+    
